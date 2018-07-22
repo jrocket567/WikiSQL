00048ba5b0bba40803f81917277cacce5933492b
jbpenick <jbpenick@192.168.1.198>
2018-07-22 10:53:41 -0400

    Baseline- working with Type and Age

--- ClassificationElso.m
+++ ClassificationElso.m
@@ -1,17 +1,19 @@
+function sqlquery=ClassificationElso(query)
 % Use text analytics to break apart queries into sections
 % not using LSTM
 % J Penick July 2018
 
-
-
-query='Maximum time on ECMO with GE pump';
+%query='Average time on ECMO for neonatal patients with VV';
+%query='Survival rate of ECMO for neonatal patients';
+%query='Maximum survival rate of ECMO for pediatric patients in 2002';
 %% Clean up input Query
 
 query=erasePunctuation(query);
 query=lower(query);
+
 Tquery=tokenizedDocument(query);
 Tquery=removeWords(Tquery,stopWords);
-Tquery=removeWords(Tquery,{'ecmo','elso'});
+Tquery=removeWords(Tquery,{'ecmo','elso','patient','patients'});
 Tquery=normalizeWords(Tquery);
 
 % maximum time ge pump
@@ -22,13 +24,7 @@ queryString=string(Tquery);
 % Conditional = > < OP >= <=
 % Syms SELECT WHERE
 
-
-
-sqlquery=['SELECT ' aggregation ' ' columnsofInterest 'FROM ' elsoDatabase 'WHERE' Conditionals];
-
-
 %% Aggregation
-
 % Renditions of MAX
 maxCommons=["best";
     "maximal";
@@ -60,39 +56,65 @@ minCommons=["minimal";
 countCommons=["Count";"total";"many"];
 
 sumCommons=["sum";"total";"summation";"addition";"add";"amount";
-"bulk";
-"quantity";
-"value";
-"worth";
-"aggregate";
-"body";
-"entirety";
-"entity";
-"epitome";
-"gross";
-"integral";
-"mass";
-"reckoning";
-"score";
-"structure";
-"summary";
-"summation";
-"synopsis";
-"system";
-"tally";
-"totality";
-"whole";
-"works"];
+    "bulk";
+    "quantity";
+    "value";
+    "worth";
+    "aggregate";
+    "body";
+    "entirety";
+    "entity";
+    "epitome";
+    "gross";
+    "integral";
+    "mass";
+    "reckoning";
+    "score";
+    "structure";
+    "summary";
+    "summation";
+    "synopsis";
+    "system";
+    "tally";
+    "totality";
+    "whole";
+    "works"];
 
 avgCommons=["avg";"mean";"average"];
 
+% Run Commons through tokenize/normalize:
+maxCommons=preProcessCommonLists(maxCommons);
+avgCommons=preProcessCommonLists(avgCommons);
+minCommons=preProcessCommonLists(minCommons);
+countCommons=preProcessCommonLists(countCommons);
+sumCommons=preProcessCommonLists(sumCommons);
 
 % See if the query matches any of the Commons
+MaxTFvec=contains(queryString,maxCommons);
+MinTFvec=contains(queryString,minCommons);
+CountTFvec=contains(queryString,countCommons);
+SumTFvec=contains(queryString,sumCommons);
+AvgTFvec=contains(queryString,avgCommons);
 
-MaxTF=sum(contains(queryString,maxCommons));
-MinTF=sum(contains(queryString,minCommons));
-CountTF=sum(contains(queryString,countCommons));
-SumTF=sum(contains(queryString,sumCommons));
+MaxTF=sum(MaxTFvec);
+MinTF=sum(MinTFvec);
+CountTF=sum(CountTFvec);
+SumTF=sum(SumTFvec);
+AvgTF=sum(AvgTFvec);
+
+% Find position and delete "used" word
+if MaxTF>=1
+    queryString=queryString(~MaxTFvec);
+elseif MinTF>=1
+    queryString=queryString(~MinTFvec);
+elseif CountTF>=1
+    queryString=queryString(~CountTFvec);
+elseif SumTF>=1
+    queryString=queryString(~SumTFvec);
+elseif AvgTF>=1
+    queryString=queryString(~AvgTFvec);
+end
+
 
 % Determine if there is any AGG portion
 if MaxTF+MinTF+CountTF+SumTF>1
@@ -105,10 +127,167 @@ elseif CountTF==1
     AggString="COUNT";
 elseif SumTF==1
     AggString="SUM";
+elseif AvgTF==1
+    AggString="AVERAGE";
 else
     AggString=' ';
 end
 
+%% Determine what columns are wanted in columnsOfInterest
 
+[columnsOfInterest, queryString]=ColumnSelectionFunction(queryString);
+
+
+%% Create WHERE conditionals
+
+% This is the only section left, so all words left in string should go to
+% some sort of WHERE conditional. Therefore loop through until all words
+% are removed from query.
+Conditional=string;
+
+% Break down by patient age
+% neonatal= <29 days
+% Pediatric= 29 days:18 yrs (6570 d)
+% Adult >18 yrs
+
+neonatalCommons=["neonatal";"neonat";"neo";"premature";"infant"];
+pediatricCommons=["pediatric";"pedi";"child";"adolescent";"ped"];
+adultCommons=["adult";"grown";"elderly";"old"];
+
+neonatalCommons=preProcessCommonLists(neonatalCommons);
+pediatricCommons=preProcessCommonLists(pediatricCommons);
+adultCommons=preProcessCommonLists(adultCommons);
+
+for idy=1:length(queryString)
+    % look for age words
+    
+    neoTFvec=contains(queryString(idy),neonatalCommons);
+    pedTFvec=contains(queryString(idy),pediatricCommons);
+    adultTFvec=contains(queryString(idy),adultCommons);
+    
+    neoTF=sum(neoTFvec);
+    pedTF=sum(pedTFvec);
+    adultTF=sum(adultTFvec);
+    
+    
+    if neoTF==1
+        Conditional=[Conditional "AGE =<28"];  %#ok<AGROW>
+    elseif pedTF==1
+        Conditional=[Conditional "AGE >28 AND AGE =<6570"]; %#ok<AGROW>
+    elseif adultTF==1
+        Conditional=[Conditional "AGE >6570"]; %#ok<AGROW>
+    end
+        
+    % look for VV or VA
+    
+    TypeCategories=["VA to VV";
+        "VA(+V)";
+        "VA+A";
+        "VA+V";
+        "VV to VA";
+        "VV(DL)";
+        "VV+V";
+        "VV+VV";
+        "VVDL+V"];
+    ParentTypeCategories=["VV";"VA"];
+    
+    typeTF=strcmpi(queryString(idy),TypeCategories);
+    parentTypeTF=strcmpi(queryString(idy),ParentTypeCategories);
+    
+    if sum(typeTF)>=1
+        selectedCat=TypeCategories(typeTF);
+        if strlength(Conditional)==0
+            Conditional=["ECMOTYPE is " selectedCat];
+        else
+            Conditional=[Conditional "AND ECMOTYPE is " selectedCat];%#ok<AGROW>
+        end
+    elseif sum(parentTypeTF)>=1
+        selectedCat=ParentTypeCategories(parentTypeTF);
+        if strlength(Conditional)==0
+            Conditional=["ECMOTYPE is " selectedCat];
+        else
+            Conditional=[Conditional "AND ECMOTYPE is " selectedCat];%#ok<AGROW>
+        end
+    end
+    
+    
+    
+end
+
+
+%% Paste it all together
+
+sqlquery=["SELECT " AggString columnsOfInterest "FROM ELSO "  "WHERE " Conditional];
+
+sqlquery=join(sqlquery);
+
+end
+
+%% Supporting Functions
+
+function outString=preProcessCommonLists(CommonList)
+
+% Run Commons through tokenize/normalize:
+mC=tokenizedDocument(CommonList);
+mC=normalizeWords(mC);
+outString=joinWords(mC);
+
+end
+
+function [outColumnList, queryString]=ColumnSelectionFunction(queryString)
+
+ECMO=evalin('base','ECMO');
+% See if we get lucky and a column name matches database
+columnNames=fieldnames(ECMO);
+columnNames=lower(columnNames);
+matches=false(length(columnNames),1);
+for idx=1:length(queryString)
+    matches=strcmp(queryString(idx),columnNames)+matches;
+end
+
+matches=logical(matches);
+matchedColumnNames=columnNames(matches);
+
+% Some common word definitions
+
+timeCommons=["time";"hours";"days";"duration"];
+timeCommons=preProcessCommonLists(timeCommons);
+
+survivalCommons=["survival";"death"];
+survivalCommons=preProcessCommonLists(survivalCommons);
+
+% Other column selections can go here.... focusing on time and survival to start.
+
+
+% See if the query matches any of the Commons
+timeTFvec=contains(queryString,timeCommons);
+survivalTFvec=contains(queryString,survivalCommons);
+
+timeTF=sum(timeTFvec);
+survivalTF=sum(survivalTFvec);
+
+% Determine if there is any columnNames portion
+if timeTF+survivalTF>1
+    ColumnNames='ColumnFailure';
+elseif timeTF==1
+    ColumnNames='Duration';
+elseif survivalTF==1
+    ColumnNames='SURV';
+elseif ~isempty(matchedColumnNames) %had a hit on a matched name. Dont select all.
+    ColumnNames=' ';
+else %Just return everything
+    ColumnNames='*';
+end
+
+% Find position and delete "used" word
+if timeTF>=1
+    queryString=queryString(~timeTFvec);
+elseif survivalTF>=1
+    queryString=queryString(~survivalTFvec);
+end
+
+outColumnList=[matchedColumnNames ColumnNames];
+
+end
 
 
--- Import_ECMO_Database.m
+++ Import_ECMO_Database.m
@@ -46,8 +46,8 @@ ECMO.APGAR1MINUTE = data(:,5);
 ECMO.APGAR5MINUTES = data(:,6);
 ECMO.EGA = data(:,7);
 ECMO.WEIGHT = data(:,8);
-ECMO.NEONATEAGEHRS = stringVectors(:,7);
-ECMO.PEDAGEMOS = stringVectors(:,8);
+ECMO.NEONATEAGEHRS = hours(str2double(stringVectors(:,7)));
+ECMO.PEDAGEMOS = calmonths(uint8((str2double(stringVectors(:,8)))));
 ECMO.PREPT = data(:,9);
 ECMO.PREFBG = data(:,10);
 ECMO.PREPLATELETS = data(:,11);
@@ -84,8 +84,8 @@ ECMO.PULMONARY = categorical(stringVectors(:,16));
 ECMO.INFECTIOUS = categorical(stringVectors(:,17));
 ECMO.METABOLIC = categorical(stringVectors(:,18));
 ECMO.TOTALVENTDAYS = categorical(stringVectors(:,19));
-ECMO.EXTUBTORADAYS = stringVectors(:,20);
-ECMO.TOTALHOSPDAYS = stringVectors(:,21);
+ECMO.EXTUBTORADAYS = str2double(stringVectors(:,20));
+ECMO.TOTALHOSPDAYS = str2double(stringVectors(:,21));
 ECMO.SURV = categorical(stringVectors(:,22));
 ECMO.NOTES = stringVectors(:,23);
 ECMO.DATEOFINTUBATION = datetime([dates{:,5}].', 'ConvertFrom', 'Excel');
@@ -101,7 +101,7 @@ ECMO.CLASSIFICATION = categorical(stringVectors(:,30));
 ECMO.ELSONUM = data(:,34);
 ECMO.UNIT = stringVectors(:,31);
 ECMO.TRANSPORT = stringVectors(:,32);
-ECMO.PUMPTYPE = stringVectors(:,33);
+ECMO.PUMPTYPE = categorical(stringVectors(:,33));
 
 % For code requiring serial dates (datenum) instead of datetime, uncomment
 % the following line(s) below to return the imported dates as datenum(s).
@@ -119,8 +119,6 @@ clearvars data raw dates stringVectors R;
 numrecords=height(ECMO);
 ECMO.ACCT=randi([111111111 999999999],numrecords,1);
 ECMO.MR=randi([11111111 99999999],numrecords,1);
-ECMO.EXTUBTORADAYS=str2double(ECMO.EXTUBTORADAYS);
-ECMO.TOTALHOSPDAYS=str2double(ECMO.TOTALHOSPDAYS);
 
 a=days(ECMO.TIMEONECMO);
 a.Format='hh:mm:ss';
@@ -130,12 +128,35 @@ a=days(ECMO.TIMEOFFECMO);
 a.Format='hh:mm:ss';
 ECMO.TIMEOFFECMO=a;
 
+ECMO.StartTime=ECMO.ONECMO+ECMO.TIMEONECMO;
+ECMO.StopTime=ECMO.OFFECMO+ECMO.TIMEOFFECMO;
+ECMO.Duration=ECMO.StopTime-ECMO.StartTime;
+
+ECMO.Age=days(ECMO.StartTime-ECMO.BD);
+
+%Create combined ECMOTYPE category
+
+ECMO.ParentECMOTYPE=ECMO.ECMOTYPE;
+ECMO.ParentECMOTYPE=mergecats(ECMO.ParentECMOTYPE,{'VVDL+V','VV(DL)','VV to VA','VV+VV','VVDL+V'},'VV');
+
 %% Create database
 
 % dbfile=fullfile(pwd,'ECMO_db.db');
 % conn=sqlite(dbfile,'create');
 
+%% Create a list of categories
+fn=fieldnames(ECMO);
+isCat=false(length(fn),1);
+for idx=1:length(fn)-3
+    isCat(idx)=iscategorical(ECMO.(fn{idx}));
+end
 
 
-
+catData=ECMO(:,isCat);
+cDfn=fieldnames(catData);
+for idn=1:width(catData)
+   
+    
+    
+end
 
